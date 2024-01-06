81a0aeed348ec755a97579965f65fc2fbeffc79d
lemonek <72318911+lemonekq@users.noreply.github.com>
2024-01-06 03:47:39 +0100

    navbar
    i have no motivation to do this, 3am

--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -1,28 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
 namespace IntelliVision
 {
-    /// <summary>
-    /// Logika interakcji dla klasy MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void NavigationChanged(object sender, RoutedEventArgs e)
+        {
+            // Neutral: #FF1B1E1E
+            // Active: #FF333838
+
+            Button ClickedButton = sender as Button;
+            if (ClickedButton != null)
+            {
+                int ButtonCount = 0;
+                foreach (FrameworkElement CurrentElement in navbar_buttons.Children)
+                {
+                    if (CurrentElement is Button)
+                    {
+                        ButtonCount++;
+                        Button CurrentButton = CurrentElement as Button;
+                        CurrentButton.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#FF1B1E1E");
+
+                        if (CurrentButton == ClickedButton)
+                        {
+                            switch (ButtonCount)
+                            {
+                                case 1:
+                                    break;
+                                case 2:
+                                    break;
+                            }
+                        }
+                    }
+                }
+
+                ClickedButton.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#FF333838");
+            }
         }
     }
 }
